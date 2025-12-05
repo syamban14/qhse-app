61cf8fb62d1d6c993bc5d598b4482203d3f742fd
syamban14 <syamsulikj.48@gmail.com>
2025-12-05 11:12:42 +0700

    menambahkan select input di component

---
+++ resources/views/components/select-input.blade.php
@@ -0,0 +1,5 @@
+@props(['disabled' => false])
+
+<select @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-qhse-primary dark:focus:border-qhse-secondary focus:ring-qhse-primary dark:focus:ring-qhse-secondary rounded-md shadow-sm']) }}>
+    {{ $slot }}
+</select>
